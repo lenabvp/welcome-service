7781329372f3062590b56ea02350b26e5d1c8b82
EC2 Default User <ec2-user@ip-172-31-18-167.us-east-2.compute.internal>
2025-06-03 04:12:01 +0000

    Added the welcome script file to fixed-script branch, fixed errors in file and pushing it

--- welcomescript.sh
+++ welcomescript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Hello 312 Student!"
-echo "Enter Your Name, please:"
+echo "Enter Your Name Please:"
 read name
-echo "$name, welcome to 312 School"
+echo "$name, welcome to 313 School"
